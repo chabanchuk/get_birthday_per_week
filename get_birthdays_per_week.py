e51d7ee34813d6846f3511aeb1abd4fee7db5998
chabanchuk <145770156+chabanchuk@users.noreply.github.com>
2023-12-03 09:20:14 +0200

    Initial commit

---
+++ get_birthdays_per_week.py
@@ -0,0 +1,24 @@
+from datetime import date, datetime
+
+
+def get_birthdays_per_week(users):
+
+
+    today_day = date.today()
+    birthdays = {}
+    for user in users:
+        user_birthday = user['birthday'].replace(year=today_day.year)
+        delta = (user_birthday - today_day).days
+        if delta < 0:
+            delta += 365
+        if 0 <= delta < 7:
+            day_week = user_birthday.strftime('%A')
+            if (day_week == 'Monday' or day_week == 'Saturday' or day_week == 'Sunday'):
+                if 'Monday' not in birthdays:
+                    birthdays['Monday'] = []
+                birthdays['Monday'].append(user['name'])
+            else:
+                if day_week not in birthdays:
+                    birthdays[day_week] = []
+                birthdays[day_week].append(user['name'])
+    return birthdays
